35bc010e1828b97e1c867d80e3bc1eb955c46e36
lukemurray7 <luke.murray@peak.bi>
2017-07-02 14:25:38 +0100

    routed

--- src/index.js
+++ src/index.js
@@ -3,22 +3,25 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-} from 'react-router-dom';
+import { BrowserRouter, Route, Switch, IndexRoute } from 'react-router-dom';
 
 import SocialMedia from './components/socialMedia';
 import Press from './components/press';
 import About from './components/about';
 import Events from './components/events';
 import Art from './components/art';
+import Home from './components/home';
 
 ReactDOM.render(
-                <Router>
-                    <Route exact path="/" component={App}>
-                    </Route>
-</Router>, document.getElementById('root'));
+  <BrowserRouter>
+    <App>
+      <Switch>
+        <Route exact path = '/' component={Home}/>
+        <Route path="/press" component={Press}/>
+        <Route path="/art" component={Art}/>
+        <Route path="/about" component={About}/>
+        <Route path="/events" component={Events}/>
+      </Switch>
+    </App>
+  </BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
