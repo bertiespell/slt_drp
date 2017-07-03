5ba06db650989f09d58239226ca3b1300f3d1d66
lukemurray7 <luke.murray@peak.bi>
2017-07-03 17:45:43 +0100

    twit feed

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import Carousel from './carousel';
+import { Timeline } from 'react-twitter-widgets'
+
+
 class Home extends Component {
     render() {
         return (
             <div>
-              <Carousel />
+                <Timeline
+                  dataSource={{
+                    sourceType: 'profile',
+                    screenName: 'SLTDRP'
+                  }}
+                  options={{
+                    username: 'SLTDRP',
+                    height: '500',
+                    width: '400'
+                  }}
+                  onLoad={() => console.log('Timeline is loaded!')}
+                  />
             </div>
         )
     }
