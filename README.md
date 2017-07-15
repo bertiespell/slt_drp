878af3aa76d1853b53dc89f6d29d2316a7ae8431
bertiespell <bernie.snell@digitalbridge.eu>
2017-07-15 13:17:30 +0100

    minor refactor

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # slt_drp
+
+Install serverless-cli globally:
+
+```
+npm install -g serverless
+```
+
+To run and deploy a build:
+
+```
+npm run build
+```
--- src/components/pressThumbnail.js
+++ src/components/pressThumbnail.js
@@ -3,7 +3,6 @@ import Thumbnail from 'react-bootstrap/lib/Thumbnail';
 import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
-import Button from 'react-bootstrap/lib/Button';
 
 class PressThumbnail extends Component {
     render() {
