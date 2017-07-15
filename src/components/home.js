7009de0bbcc7b1262472085da482395b4a358f73
lukemurray7 <luke.murray@peak.bi>
2017-07-15 12:53:57 +0100

    test

--- src/components/home.js
+++ src/components/home.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
 import Carousel from './carousel';
 import { Timeline } from 'react-twitter-widgets'
+import Events from './events';
+import FacebookProvider, { Feed  } from 'react-facebook';
+import FB from 'fb';
 
 
 class Home extends Component {
+  componentDidMount() {
+    window.fbAsyncInit = function() {
+  FB.init({
+    appId            : '116022249025281',
+    autoLogAppEvents : true,
+    xfbml            : true,
+    version          : 'v2.9'
+  });
+  FB.AppEvents.logPageView();
+};
+
+}
+componentDidUpdate() {
+     FB.XFBML.parse();
+}
     render() {
         return (
-            <div>
-                <Timeline
+          <div>
+            <Carousel />
+            <div className="things">
+              <div className="thing">
+                <Events />
+              </div>
+              <div className="thing">
+                <Timeline className="thing"
                   dataSource={{
                     sourceType: 'profile',
                     screenName: 'SLTDRP'
@@ -19,7 +43,14 @@ class Home extends Component {
                   }}
                   onLoad={() => console.log('Timeline is loaded!')}
                   />
+              </div>
+
+                <div className="fb-page thing" data-href="https://www.facebook.com/slutdropleeds" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/slutdropleeds" className="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/slutdropleeds">SLUT DROP</a></blockquote></div>
             </div>
+          </div>
+
+
+
         )
     }
 }
